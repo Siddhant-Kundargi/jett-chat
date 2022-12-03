d6caebc3f844a57dcb0e84860bbe4437aa789391
Siddhant Kundargi <sidkundargi@gmail.com>
2022-12-03 23:58:07 +0530

    basic if statement on database init

--- jett_chat/DBfn/initialiseDatabase.py
+++ jett_chat/DBfn/initialiseDatabase.py
@@ -10,23 +10,35 @@ def initialize_database():
 
     mycursor = mydb.cursor()
 
-    mycursor.execute("create database user")
-    mycursor.execute("use user")
-    mycursor.execute("""CREATE TABLE UserInfo(name varchar(50),
-            email varchar(40), 
-            phone varchar(15), 
-            uname varchar(30),
-            PRIMARY KEY (uname))
-            """)
-    mycursor.execute("""CREATE TABLE Token(uname varchar(30), 
-            token varchar(160),
-            PRIMARY KEY (uname), 
-            FOREIGN KEY (uname) REFERENCES UserInfo(uname))
-            """)
-    mycursor.execute("""CREATE TABLE Password(uname varchar(30),
-            password varchar(20),
-            PRIMARY KEY (uname), 
-            FOREIGN KEY (uname) REFERENCES UserInfo(uname))
-            """)
+    mycursor.execute("SHOW DATABASES")
+    list_of_databases = mycursor.fetchall()
+
+    if not ('user',) in list_of_databases:
+
+        print("database not found, creating one")
+
+        mycursor.execute("create database user")
+        mycursor.execute("use user")
+        mycursor.execute("""CREATE TABLE UserInfo(name varchar(50),
+                email varchar(40), 
+                phone varchar(15), 
+                uname varchar(30),
+                PRIMARY KEY (uname))
+                """)
+        mycursor.execute("""CREATE TABLE Token(uname varchar(30), 
+                token varchar(160),
+                PRIMARY KEY (uname), 
+                FOREIGN KEY (uname) REFERENCES UserInfo(uname))
+                """)
+        mycursor.execute("""CREATE TABLE Password(uname varchar(30),
+                password varchar(20),
+                PRIMARY KEY (uname), 
+                FOREIGN KEY (uname) REFERENCES UserInfo(uname))
+                """)
+        
+        print("Database Created")
+
+    else:
+        print("Database Already Exists")
 
 initialize_database()
