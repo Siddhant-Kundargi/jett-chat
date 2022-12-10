af83d94524b982323ca9d75883ac25079f7067f2
Siddhant Kundargi <sidkundargi@gmail.com>
2022-12-10 11:07:06 +0530

    initial commit for github

--- runserver.py
+++ runserver.py
@@ -2,9 +2,9 @@ from os import environ
 from jett_chat import app
 
 if __name__ == '__main__':
-    # HOST = "0.0.0.0"
-    # try:
-    #     PORT = int(environ.get('SERVER_PORT', '5555'))
-    # except ValueError:
-    #     PORT = 5555
-    app.run(host='0.0.0.0', port=5555)
+    HOST = "0.0.0.0"
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(host=HOST, port=PORT)
